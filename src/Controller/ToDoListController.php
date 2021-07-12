d72e615bea53522d251c62c84633e6b0531dccf5
Gabriel Cristea <gabriel.cristea@mountsoftware.ro>
2021-07-12 14:15:49 +0300

    first

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    /**
+     * @Route("/", name="to_do_list")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create(): Response
+    {
+        exit('to do: create a new task!');
+    }
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id): Response
+    {
+        exit('to do: switch status of the task!'. $id);
+    }
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete($id): Response
+    {
+        exit('to do: delete a task with the id of!'. $id);
+    }
+
+}
